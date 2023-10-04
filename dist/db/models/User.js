97cb6e3d0b72df9491dda7f358c782622cd6f55e
Vahé Krikorian <vahekrikorian@MacBook-Pro.local>
2023-10-04 15:15:00 +0200

    update

--- dist/db/models/User.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Users = void 0;
-const mongo_1 = require("../mongo");
-exports.Users = mongo_1.db.collection('users');
-//# sourceMappingURL=User.js.map
