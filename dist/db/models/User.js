41f38b27af0b199affb8a883ffa13ad8c4f7ca1f
ThomasCast3 <castanier.thomas0703@gmail.com>
2023-10-04 15:13:19 +0200

    clean project commit

---
+++ dist/db/models/User.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Users = void 0;
+const mongo_1 = require("../mongo");
+exports.Users = mongo_1.db.collection('users');
+//# sourceMappingURL=User.js.map
---
+++ front/src/components/HomeIdle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1 class="green">Hello Idle Game</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+</style>
